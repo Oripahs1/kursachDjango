657a80043fa8100d8b8853634459d7a865fce02e
German <gamer.mel@mail.ru>
2024-03-11 21:48:41 +1000

    Допиленный парсер, который по ссылке собирает все данные со страницы в экземпляр класса

--- parser.py
+++ parser.py
@@ -1,29 +1,92 @@
+# Если собрался что-то менять - спроси Германа
+# Тут мы парсим данные со странички по ссылке
+#
+# Есть два класса:
+# Parser - включает в себя логику и сам парсер
+#
+# Car_data - инициализирует процесс парсинга и собирает в себя данные
+#
+# В идеале в конце должен быть экземпляр класса Car_data, со всеми нужными данными внутри
+
 from bs4 import BeautifulSoup
 import requests
-import numpy as np
+
+
+
+class Parser(object):
+    html_page = None
+
+    def __init__(self, url):
+
+        response = requests.get(url)
+        self.html_page = BeautifulSoup(response.text, "lxml")
+        print(response)
+
+    def parse_title(self):
+
+        name = self.html_page.find('div', 'page_title').text
+        return name
+
+    def parse_auction_data(self):
+
+        auction_data = self.html_page.find('div', 'col_left').text
+        auction_data = auction_data.split('\n')
+        return auction_data
+
+    def parse_car_options(self):
+
+        car_options = self.html_page.find('div', 'car_description')
+        car_options = car_options.find_all('div', 'car_option')
+        form_data = [[0] * 2 for i in range(len(car_options))]
+
+        # Условие не трогать, работает и слава богу
+        k = 0
+        for el in range(0, len(car_options)):
+            split_data_of_car = car_options[el].text.split()
+            form_data[k][0] = split_data_of_car[0]
+            form_data[k][1] = split_data_of_car[1]
+            k += 1
+        car_options = form_data
+        return car_options
+
+    def parse_content(self):
+
+        content = self.html_page.find('div', 'content')
+        content = content.find_all('td')
+        form_data = [[0] * 2 for i in range(len(content) // 2)]
+
+        # Почему здесь по-другому, не знаю, но тоже работает и слава богу
+        k = 0
+        for el in range(0, len(content), 2):
+            form_data[k][0] = content[el].text
+
+            form_data[k][1] = content[el + 1].text
+            k += 1
+
+        content = form_data
+        return content
+
+class Car_data(object):
+    url = ''
+    title = ''
+    auction_data = ''
+    car_options = ''
+    content = ''
+
+    def __init__(self, url):
+
+        parser = Parser(url)
+
+        self.title = parser.parse_title()
+        self.auction_data = parser.parse_auction_data()
+        self.car_options = parser.parse_car_options()
+        self.content = parser.parse_content()
+
+    def print(self):
+
+        print([self.title, self.auction_data, self.car_options, self.content])
 
 url = 'https://www.carwin.ru/japanauc/see/945389787'
-response = requests.get(url)
-print(response)
-bs = BeautifulSoup(response.text, "lxml")
+Obj = Car_data(url)
+Obj.print()
 
-temp = bs.find('div', 'content')
-temp = temp.find_all('td')
-
-FormData = [[0]*2 for i in range(len(temp)//2)]
-
-print(FormData[1][1])
-
-k = 0
-print(range(len(temp), 2))
-print(temp[0].text)
-for el in range(0, len(temp), 2):
-
-    FormData[k][0] = temp[el].text
-
-    FormData[k][1] = temp[el+1].text
-    k += 1
-
-
-
-print(np.matrix(FormData))
