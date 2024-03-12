62fd6f6042039c6a60a77c0a7a9a485b36937e69
oripahs <shapiro.ad@mail.ru>
2024-03-12 23:41:48 +1000

    чутка отредачил код в views.parser

--- auction/views.py
+++ auction/views.py
@@ -14,6 +14,7 @@ from .forms import ParserForm
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+
 # class CarPageView(TemplateView):
 #     template_name = "car.html"
 
@@ -35,10 +36,8 @@ class ParserPageView(TemplateView):
                 return render(request, "car.html",
                               {'title': Obj.title, 'auction_data': Obj.auction_data, 'car_options': Obj.car_options,
                                'content': Obj.content, 'range': range(4)})
-                # return HttpResponseRedirect('/')
         else:
             form = ParserForm()
-
         return render(request, 'parser.html', {'form': form})
 
 
@@ -54,46 +53,33 @@ class Parser(object):
         print(response)
 
     def parse_title(self):
-
         name = self.html_page.find('div', 'page_title').text
         return name
 
     def parse_auction_data(self):
-
         auction_data = self.html_page.find('div', 'col_left').text
-        auction_data = auction_data.split('\n')
+        auction_data = [value for value in auction_data.split('\n') if value != '']
         return auction_data
 
     def parse_car_options(self):
-
         car_options = self.html_page.find('div', 'car_description')
         car_options = car_options.find_all('div', 'car_option')
-        form_data = [[0] * 2 for i in range(len(car_options))]
+        form_data = list()
 
         # Условие не трогать, работает и слава богу
-        k = 0
         for el in range(0, len(car_options)):
             split_data_of_car = car_options[el].text.split()
-            form_data[k][0] = split_data_of_car[0]
-            form_data[k][1] = split_data_of_car[1]
-            k += 1
+            form_data.append([split_data_of_car[0], split_data_of_car[1]])
         car_options = form_data
         return car_options
 
     def parse_content(self):
-
         content = self.html_page.find('div', 'content')
         content = content.find_all('td')
-        form_data = [[0] * 2 for i in range(len(content) // 2)]
-
+        form_data = list()
         # Почему здесь по-другому, не знаю, но тоже работает и слава богу
-        k = 0
         for el in range(0, len(content), 2):
-            form_data[k][0] = content[el].text
-
-            form_data[k][1] = content[el + 1].text
-            k += 1
-
+            form_data.append([content[el].text, content[el + 1].text])
         content = form_data
         return content
 
@@ -107,14 +93,14 @@ class Car_data(object):
 
     def __init__(self, url):
         parser = Parser(url)
-
         self.title = parser.parse_title()
         self.auction_data = parser.parse_auction_data()
         self.car_options = parser.parse_car_options()
         self.content = parser.parse_content()
 
     def print(self):
-        print([self.title, self.auction_data, self.car_options, self.content])
+        print('название машины', self.title, 'аукцион', self.auction_data, 'основное про машину', self.car_options,
+              'таблица', self.content, sep='\n', end='\n')
 
 # url = 'https://www.carwin.ru/japanauc/see/945389787'
 # Obj = Car_data(url)
