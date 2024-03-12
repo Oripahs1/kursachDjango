151b56f8acf16c463d7f3a8ee8e6760d7a886826
oripahs <shapiro.ad@mail.ru>
2024-03-13 01:35:39 +1000

    закомментировал модели потому что нужны миграции. миграции после обсуждения с тимой

--- auction/models.py
+++ auction/models.py
@@ -1,54 +1,54 @@
 from django.db import models
 
-class Invoices(models.Model):
-    id_invoice = models.AutoField(primary_key=True)
-    payer = models.TextField()
-    receipent = models.TextField()
-    date_form = models.DateField()
-    date_pay = models.DateField()
-    sum = models.IntegerField()
-    check = models.TextField()
-    type = models.CharField(max_length=50)  # Уточните ENUM-тип
-
-class Trans(models.Model):
-    id_trans = models.AutoField(primary_key=True)
-    id_invoice = models.ForeignKey(Invoices, on_delete=models.CASCADE)
-    trans_comp = models.TextField()
-    departure_point = models.TextField()
-    destination_point = models.TextField()
-    date_form = models.DateField()
-    date_shipment = models.DateField()
-    date_receive = models.DateField()
-
-class Order(models.Model):
-    id_order = models.AutoField(primary_key=True)
-    id_customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
-    id_worker = models.ForeignKey('Worker', on_delete=models.CASCADE)
-    id_invoice_comp = models.ForeignKey(Invoices, on_delete=models.CASCADE)
-    status = models.TextField()
-    date_start = models.DateField()
-    date_end = models.DateField()
-    comment = models.TextField()
-    id_car = models.ForeignKey('Car', on_delete=models.CASCADE)
-
-class Customer(models.Model):
-    id_customer = models.AutoField(primary_key=True)
-    full_name = models.TextField()
-    passport_num = models.TextField()
-    tel_num = models.TextField()
-
-class Car(models.Model):
-    id_car = models.AutoField(primary_key=True)
-    auc_link = models.TextField()
-    name = models.TextField()
-    price_start = models.IntegerField()
-    price_buy = models.IntegerField()
-    auc_list = models.TextField()
-    SBTS = models.TextField()
-    PTD = models.TextField()
-    auc_buy_date = models.DateField()
-
-class Worker(models.Model):
-    id_worker = models.AutoField(primary_key=True)
-    full_name = models.TextField()
-    job_title = models.TextField()
+# class Invoices(models.Model):
+#     id_invoice = models.AutoField(primary_key=True)
+#     payer = models.TextField()
+#     receipent = models.TextField()
+#     date_form = models.DateField()
+#     date_pay = models.DateField()
+#     sum = models.IntegerField()
+#     check = models.TextField()
+#     type = models.CharField(max_length=50)  # Уточните ENUM-тип
+#
+# class Trans(models.Model):
+#     id_trans = models.AutoField(primary_key=True)
+#     id_invoice = models.ForeignKey(Invoices, on_delete=models.CASCADE)
+#     trans_comp = models.TextField()
+#     departure_point = models.TextField()
+#     destination_point = models.TextField()
+#     date_form = models.DateField()
+#     date_shipment = models.DateField()
+#     date_receive = models.DateField()
+#
+# class Order(models.Model):
+#     id_order = models.AutoField(primary_key=True)
+#     id_customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+#     id_worker = models.ForeignKey('Worker', on_delete=models.CASCADE)
+#     id_invoice_comp = models.ForeignKey(Invoices, on_delete=models.CASCADE)
+#     status = models.TextField()
+#     date_start = models.DateField()
+#     date_end = models.DateField()
+#     comment = models.TextField()
+#     id_car = models.ForeignKey('Car', on_delete=models.CASCADE)
+#
+# class Customer(models.Model):
+#     id_customer = models.AutoField(primary_key=True)
+#     full_name = models.TextField()
+#     passport_num = models.TextField()
+#     tel_num = models.TextField()
+#
+# class Car(models.Model):
+#     id_car = models.AutoField(primary_key=True)
+#     auc_link = models.TextField()
+#     name = models.TextField()
+#     price_start = models.IntegerField()
+#     price_buy = models.IntegerField()
+#     auc_list = models.TextField()
+#     SBTS = models.TextField()
+#     PTD = models.TextField()
+#     auc_buy_date = models.DateField()
+#
+# class Worker(models.Model):
+#     id_worker = models.AutoField(primary_key=True)
+#     full_name = models.TextField()
+#     job_title = models.TextField()
