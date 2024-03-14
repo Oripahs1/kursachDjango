6d35b00d5617af055c08bca5a10581eb72669e57
oripahs <shapiro.ad@mail.ru>
2024-03-14 16:23:31 +1000

    проверка

--- auction/urls.py
+++ auction/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('parser/', views.ParserPageView.as_view(), name='parser'),
+    path('login/', views.LoginPageView.as_view(), name='login'),
+    path('logout/', views.LogoutPageView.as_view(), name='logout'),
+    path('registration/', views.RegistrationPageView.as_view(), name='registration'),
     # path('car/', views.CarPageView.as_view(), name='parser'),
 
 ]
--- auction/views.py
+++ auction/views.py
@@ -15,6 +15,18 @@ class HomePageView(TemplateView):
     template_name = "home.html"
 
 
+class LoginPageView(TemplateView):
+    template_name = "login.html"
+
+
+class LogoutPageView(TemplateView):
+    template_name = "logout.html"
+
+
+class RegistrationPageView(TemplateView):
+    template_name = "registration/"
+
+
 # class CarPageView(TemplateView):
 #     template_name = "car.html"
 
@@ -152,7 +164,6 @@ class Parser(object):
         return content
 
     def parse_image(self):
-
         image = self.html_page.find('div', 'my-gallery')
         image = image.find_all('img')
         form_data = list()
@@ -163,7 +174,6 @@ class Parser(object):
         return image
 
     def parse_auc_list(self):
-
         auc_list = self.html_page.find('div', 'scheme_block')
         auc_list = auc_list.find('img')
         auc_list = auc_list['src']
@@ -257,20 +267,21 @@ class Car_data(object):
 
         self.auc_list = parser.parse_auc_list()
 
-
     def print(self):
         print('название машины', self.title, 'аукцион', self.auction_data, 'основное про машину', self.car_options,
               'таблица', self.content, sep='\n', end='\n')
         print('картинки', self.image, end='\n')
         print(self.auc_name, self.auc_number, self.auc_date, sep='\n', end='\n')
-        print(self.year_car, self.mileage, self.color, self.options, self.the_body, self.volume, self.cpp, self.estimation, sep='\n', end='\n')
+        print(self.year_car, self.mileage, self.color, self.options, self.the_body, self.volume, self.cpp,
+              self.estimation, sep='\n', end='\n')
         print(self.cooling, self.condition, self.fuel, self.equipment)
 
     def __del__(self):
         print('Удален')
 
     def save_me_to_bd(self):
-        new_car = CarForPage.objects.create(title=self.title, auction_data=self.auction_data, content=self.content, car_options=self.car_options)
+        new_car = CarForPage.objects.create(title=self.title, auction_data=self.auction_data, content=self.content,
+                                            car_options=self.car_options)
         new_car_new = Car.objects.create(
             auc_link=self.auc_link,
             title=self.title,
@@ -299,9 +310,7 @@ class Car_data(object):
             deadline_for_the_price_offer=self.deadline_for_the_price_offer,
             day_of_the_event=self.day_of_the_event,
             number_of_sessions=self.number_of_sessions,
-            auc_list = self.auc_list
+            auc_list=self.auc_list
         )
         print(new_car)
         print(new_car_new)
-
-
